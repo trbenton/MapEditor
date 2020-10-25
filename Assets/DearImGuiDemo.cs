52040462634a223121fb166882a938cb2305be09
trbenton <trbenton@users.noreply.github.com>
2020-10-25 13:34:44 -0400

    add imgui to the project

---
+++ Assets/DearImGuiDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using ImGuiNET;
+
+public class DearImGuiDemo : MonoBehaviour
+{
+    protected void OnEnable()
+    {
+        ImGuiUn.Layout += OnLayout;
+    }
+
+    protected void OnDisable()
+    {
+        ImGuiUn.Layout -= OnLayout;
+    }
+
+    protected void OnLayout()
+    {
+        ImGui.ShowDemoWindow();
+    }
+}
